2ade41598187562134219ad47459695e28777509
jhosuapp <jhosuapvll@gmail.com>
2025-05-27 18:02:34 -0500

    feat: schemas

--- src/shared/PrivateLayout.tsx
+++ src/shared/PrivateLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const PrivateLayout = ():React.JSX.Element => {
     return (
         <main>
-            <section className="flex justify-center fixed top-0 left-0 w-full bg-primary py-5">
+            <section className="flex justify-center fixed top-0 left-0 w-full bg-primary py-5 z-50">
                 <p className="text-white">Private layout</p>
             </section>
             <Outlet />
--- src/shared/PublicLayout.tsx
+++ src/shared/PublicLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const PublicLayout = ():React.JSX.Element => {
     return (
         <main>
-            <section className="flex justify-center fixed top-0 left-0 w-full bg-white py-5">
+            <section className="flex justify-center fixed top-0 left-0 w-full bg-white py-5 z-50">
                 <p className="text">Public layout</p>
             </section>
             <Outlet />
